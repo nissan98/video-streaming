269d69c96415fcfdbd4b3624174f3ea18be34c4c
nissanDesktop <nishantanathnikita@gmail.com>
2025-08-14 23:45:07 +0530

    i broke the code

--- controllers/video.controllers.js
+++ controllers/video.controllers.js
@@ -1,7 +1,6 @@
 import { videoModel } from "../schemas/video.schema.js"
 
 const getVideos = async (req, res) => {
-
   const { _id } = req.userData
   const videos = await videoModel.aggregate([
     {
